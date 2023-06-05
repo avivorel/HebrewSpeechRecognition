4f406742be22e2a9a4afa5c520340158a0d1238f
avivorel <oravivel@gmail.com>
2023-06-05 13:14:38 +0300

    Speech to text is working

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
 const App = () => {
+    const [isListening, setIsListening] = useState(false);
     const {
         transcript,
         listening,
@@ -9,18 +10,100 @@ const App = () => {
         browserSupportsSpeechRecognition
     } = useSpeechRecognition();
 
+    useEffect(() => {
+        if (transcript.includes('לחזור')) {
+            // Add your logic to highlight or indicate the word "לחזור" in the transcript
+            console.log('Word "לחזור" detected!');
+        }
+    }, [transcript]);
+
+    const startListening = () => {
+        setIsListening(true);
+        SpeechRecognition.startListening({ continuous: true });
+    };
+
+    const stopListening = () => {
+        setIsListening(false);
+        SpeechRecognition.stopListening();
+    };
+
+    const checkWordRecognition = () => {
+        if (transcript.includes('לחזור')) {
+            console.log('Word "לחזור" detected!');
+        }
+    };
+
+    const handleSpeechRecognitionEnd = () => {
+        setIsListening(false);
+        checkWordRecognition();
+    };
+
     if (!browserSupportsSpeechRecognition) {
         return <span>Browser doesn't support speech recognition.</span>;
     }
 
     return (
-        <div>
-            <p>Microphone: {listening ? 'on' : 'off'}</p>
-            <button onClick={SpeechRecognition.startListening}>Start</button>
-            <button onClick={SpeechRecognition.stopListening}>Stop</button>
-            <button onClick={resetTranscript}>Reset</button>
-            <p>{transcript}</p>
+        <div style={styles.container}>
+            <h1 style={styles.heading}>Audio Recognition</h1>
+            <div style={styles.microphoneStatus}>
+                <p>Microphone: {listening ? 'on' : 'off'}</p>
+            </div>
+            <div style={styles.controls}>
+                <button onClick={startListening} disabled={listening} style={styles.button}>
+                    Start
+                </button>
+                <button onClick={stopListening} disabled={!listening} style={styles.button}>
+                    Stop
+                </button>
+                <button onClick={resetTranscript} style={styles.button}>
+                    Reset
+                </button>
+            </div>
+            <div style={styles.transcript}>
+                {transcript.split(' ').map((word, index) => (
+                    <span key={index} style={{ background: word === 'לחזור' ? 'yellow' : 'transparent' }}>
+            {word}{' '}
+          </span>
+                ))}
+            </div>
         </div>
     );
+};
+
+const styles = {
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        margin: '2rem',
+    },
+    heading: {
+        marginBottom: '1rem',
+    },
+    microphoneStatus: {
+        marginBottom: '1rem',
+    },
+    controls: {
+        marginBottom: '1rem',
+    },
+    button: {
+        marginRight: '1rem',
+        padding: '0.5rem 1rem',
+        fontSize: '1rem',
+        backgroundColor: '#4285F4',
+        color: '#fff',
+        border: 'none',
+        borderRadius: '4px',
+        cursor: 'pointer',
+        outline: 'none',
+    },
+    transcript: {
+        backgroundColor: '#f5f5f5',
+        padding: '1rem',
+        borderRadius: '4px',
+        minHeight: '100px',
+        overflow: 'auto',
+        width: '400px',
+    },
 };
 export default App;
